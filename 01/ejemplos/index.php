5bf162076f5cc3dd2c27744040f2525c79e493c6
Saúl Saranova <saul@dewenir.es>
2021-06-15 16:18:50 +0200

    Ejemplos PHP básico

--- 01/ejemplos/index.php
+++ 01/ejemplos/index.php
@@ -1,1 +1,31 @@
-<?php echo '<strong>Hola mundo</strong>'; ?>
+<?php
+echo '<strong>Hola mundo</strong>';
+
+$mi_variable = '0';
+var_dump($mi_variable);
+var_dump((int) $mi_variable);
+var_dump(intval($mi_variable));
+
+$miArray = ['uno', 'dos', 'tres'];
+var_dump($miArray);
+
+$miArray[0] = 'UNODOESTY';
+echo $miArray[0] . '<br />';
+
+for ($i = 0; $i < count($miArray); $i++) {
+    echo $miArray[$i] . '<br />';
+}
+
+foreach ($miArray as $value) {
+    var_dump($value);
+}
+
+$miArrayAsociativo = array(
+    'Nombre' => 'Fulanito',
+    'Apellido' => 'Apellido fulanito',
+    'DNI' => '12345678A'
+);
+
+foreach ($miArrayAsociativo as $key => $value) {
+    var_dump($key . ': ' . $value);
+}
