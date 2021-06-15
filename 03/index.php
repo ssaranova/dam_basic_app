4ac6ba583f7a231657b3e0bc5f76f8fb8a43b1da
Saúl Saranova <saul@dewenir.es>
2021-06-15 18:57:23 +0200

    Tema 03 - Ejemplos

---
+++ 03/index.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * Variables a usar en los templates
+     */
+    $title = 'Mi página principal';
+?>
+
+<?php require_once './includes/partials/header.php'; ?>
+<section>
+    <article>
+        <h2>Este es un contenido dummy, para mostrar en la homepage.</h2>
+    </article>
+</section>
+<?php require_once './includes/partials/footer.php'; ?>
