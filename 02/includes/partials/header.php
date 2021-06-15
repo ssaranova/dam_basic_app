ed798273c19e424bd46f2d0d1e5542f325fabacd
Saúl Saranova <saul@dewenir.es>
2021-06-15 17:32:54 +0200

    Ejercicio 02

---
+++ 02/includes/partials/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <meta name="author" content="Saúl" />
+    <meta name="description" content="Gestión de usuarios básica para practicar PHP, CSS y JS" />
+    <title>Mi aplicación</title>
+    <link rel="stylesheet" href="./assets/css/main.css" />
+</head>
+
+<body>
+    <header>
+        <h1><?= $title ?? 'Inicio'; ?></h1>
+    </header>
+    <main>
